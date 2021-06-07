f80c0b23d5f4eb6f2a2cb5dfbddede3e92777213
stevexdbr <steve.marin.arboleda@gmail.com>
2021-06-07 18:10:29 -0500

    ALPHA GYS

---
+++ source/OutdatedSubState.hx
@@ -0,0 +1,45 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	public static var needVer:String = "IDFK LOL";
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"Si te sale esto no te asustes no hagas caso a las notas\n"
+			+ MainMenuState.kadeEngineVer
+			+ " La Nueva Versión de Kade es: " + needVer
+			+ "!\nAplasta ESC para skipear esto",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			FlxG.openURL("https://github.com/KadeDev/Kade-Engine/releases/latest");
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
